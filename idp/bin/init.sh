704f254a5638019f5fc2238d9bf27099fda9bb7b
Komura Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2022-02-26 23:28:46 +0900

    disable sshd

--- idp/bin/init.sh
+++ idp/bin/init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # OpenSSHを起動する。
-echo "$SSH_PASSWD" | chpasswd
-/usr/sbin/sshd &
+#echo "$SSH_PASSWD" | chpasswd
+#/usr/sbin/sshd &
 
 # Jettyを起動する。
 $JAVA_HOME/bin/java -jar $JETTY_HOME/start.jar $JETTY_JAVA_ARGS
